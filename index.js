a909c9d4113f46f6f4c5f51ae0f90ef0916688ff
mashuxa <mashuxa@tut.by>
2018-08-02 18:09:20 +0300

    start template

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
+var actionsArray = [
+    function (next) {
+            setTimeout(function () {
+                next(null, '5000ms'); 
+                console.log('5000ms');
+            }, 5000); 
+            
+        },
+    function (next) {
+            setTimeout(function () {
+                next(null, '500ms'); 
+                console.log('500ms');
+            }, 500);
+        },
+    function (next) {
+            setTimeout(function () {
+                next(null, '1000ms'); 
+                console.log('1000ms');
+            }, 1000);
+        }
+    ];
 
+function completeTest() {  
+        console.info('OK!');  
+}
 
-function parallel(arr, callback){
+function next(){
+    
+}
+
+var result = [];
+function parallel(arr, callback) {
+//    var result = [];
+    arr.forEach(function (action, index){
+        try {
+            var actionResult = action(next);
+//когда функция выполнится записать результат в массив результатов и вызвать функцию next(null, actionResult)
+            result[index] = actionResult;
+        } catch(error) {
+            console.log(error);
+//когда функция выполнится с ошибкой вызвать функцию next(error);   
+        }         
+    });
+    console.log(result);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+parallel(actionsArray, completeTest);
