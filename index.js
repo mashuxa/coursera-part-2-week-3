0842812bfd62d886c4556d9624a9638caa2fa65f
mashuxa <mashuxa@tut.by>
2018-08-03 16:58:09 +0300

    start template

--- index.js
+++ index.js
@@ -1,75 +1,59 @@
 var actionsArray = [
     function (next) {
-            setTimeout(function () {
-                next(null, '5000ms'); 
-                console.log('5000ms');
-            }, 5000); 
-            
-        }, 
-    function (next) {
-            setTimeout(function () {
-                next(null, '500ms'); 
-                console.log('500ms');
-            }, 500);
+        setTimeout(function () {
+            next(null, '5000ms');
+        }, 5000);
+
         },
     function (next) {
-            setTimeout(function () {
-                next(null, '1000ms'); 
-                console.log('1000ms');
-            }, 1000);
+        setTimeout(function () {
+            next(null, '500ms');
+        }, 500);
         },
     function (next) {
-            setTimeout(function () {
-                next('ERROR');
-            }, 2000);
-        },
-    function (next) {
-            setTimeout(function () {
-                next('ERROR');
-            }, 3000);
+        setTimeout(function () {
+            next(null, '1000ms');
+        }, 1000);
         }
+//    ,
+//    function (next) {
+//        setTimeout(function () {
+//            next('ERROR');
+//        }, 2000);
+//        },
+//    function (next) {
+//        setTimeout(function () {
+//            next('ERROR');
+//        }, 3000);
+//        }
     ];
 
-function completeTest() {  
-        console.info('OK!');  
+function completeTest() {
+    console.info('OK!');
 }
 
 
 
- 
+
 function parallel(arr, callback) {
     var result = [];
-
+    var counter = 0;
 
     arr.forEach(function (action, index) {
         function next(obj, data) {
             if (obj === null) {
                 result[index] = data;
-                console.log(result);
+                counter++; 
+                if(counter === arr.length) {
+                    callback(null, result); 
+                }
             } else {
-                console.log(obj);
-                //вызвать один раз callback(error) с первой ошибкой
-                //callback(error);
+                callback(error);
             }
         }
         action(next);
     });
-
-
-
-    //когда ВСЕ операции завершились 
-    //callback(null, result);
-
-
-
-
 }
-
-
-
-
-
-
 
 
 
